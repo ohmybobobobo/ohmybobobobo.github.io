983b74d765194119fae2a9d1a8cbce83b78a5dc0
GUO BAOYI <1155112753@gmai.com>
2018-11-05 20:19:27 +0800

    update

--- popitems.js
+++ popitems.js
@@ -43,4 +43,41 @@ $(document).ready(function(){
       }); // end .getJSON
  }); // end button
 
+ $('#B1').on('click', function() {
+  var table2_items = [];
+   var i = 0;
+   var airtable_read_endpoint = "https://api.airtable.com/v0/app9lhFw2d0zCNuUL/Stores?api_key=keyzw1peVZWig6wbC&view=Grid%20view";
+   var table2_dataSet = [];
+   $.getJSON(airtable_read_endpoint, function(result) {
+          $.each(result.records, function(key,value) {
+              table2_items = [];
+                  table2_items.push(value.fields.stores_);
+                  table2_items.push(value.fields.location);
+                  table2_items.push(value.fields.telephone);
+                  table2_items.push(value.fields.business_hours);
+                  table2_items.push(value.fields.items_provided);
+                  table2_dataSet.push(table2_items);
+                  console.log(table2_items);
+           }); // end .each
+
+
+        $('table#example2').DataTable( {
+            data: table2_dataSet,
+            retrieve: true,
+            columns: [
+                { title: "stores_",
+                  defaultContent:""},
+                { title: "location",
+                    defaultContent:"" },
+                { title: "telephone",
+                  defaultContent:"" },
+                { title: "business_hours",
+                  defaultContent:""},
+                { title: "items_provided",
+                    defaultContent:""},
+            ]
+        } );
+   }); // end .getJSON
+}); // end button
+
 }); // document ready
