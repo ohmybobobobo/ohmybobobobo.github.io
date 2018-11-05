8f5f88a4fb064c2ed21382df2fb2b28ddb5a3af9
GUO BAOYI <1155112753@gmai.com>
2018-11-05 21:38:12 +0800

    update

--- popitems.js
+++ popitems.js
@@ -44,7 +44,7 @@ $(document).ready(function(){
  }); // end button
 
  $('#B1').on('click', function() {
-  var table2_items = [];
+   var table2_items = [];
    var i = 0;
    var airtable_read_endpoint = "https://api.airtable.com/v0/app9lhFw2d0zCNuUL/Stores?api_key=keyzw1peVZWig6wbC&view=Grid%20view";
    var table2_dataSet = [];
@@ -52,14 +52,11 @@ $(document).ready(function(){
           $.each(result.records, function(key,value) {
               table2_items = [];
                   table2_items.push(value.fields.stores_);
-                  table2_items.push(value.fields.location);
-                  table2_items.push(value.fields.telephone);
-                  table2_items.push(value.fields.business_hours);
                   table2_items.push(value.fields.items_provided);
                   table2_dataSet.push(table2_items);
                   console.log(table2_items);
            }); // end .each
-
+                  console.log(table2_dataSet);
 
         $('table#example2').DataTable( {
             data: table2_dataSet,
@@ -67,16 +64,25 @@ $(document).ready(function(){
             columns: [
                 { title: "stores_",
                   defaultContent:""},
-                { title: "location",
-                    defaultContent:"" },
-                { title: "telephone",
-                  defaultContent:"" },
-                { title: "business_hours",
-                  defaultContent:""},
                 { title: "items_provided",
                     defaultContent:""},
             ]
         } );
+
+                      var chart = c3.generate({
+                      binto: {chart},
+                      data: {
+                      columns: table2_dataSet,
+                      type : 'bar'
+                      },
+                      axis: {
+                      x: {label: 'stores_'},
+                      y: {label: 'items_provided'}
+                      },
+                      bar: {
+                      title: "numbers of items",
+                      }
+                      });
    }); // end .getJSON
 }); // end button
 
